e040faa27c579bb7a1402142ed9157a94decec14
PedroHMenezes <49311416+PedroHMenezes@users.noreply.github.com>
2019-05-03 21:58:14 -0300

    Cores e dimensoes do jogo

---
+++ Cores.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 21:57:06 2019
+
+@author: User
+"""
+#Cores
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# Configurações do jogo
+WIDTH = 1024   
+HEIGHT = 768 
+FPS = 60
+TITLE = "Tilemap Demo"
+BGCOLOR = DARKGREY
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
