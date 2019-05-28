8273c5aaacd3071112c83d574b997a93633bf987
PedroHMenezes <49311416+PedroHMenezes@users.noreply.github.com>
2019-05-28 10:09:33 -0300

    Mudanças para inserir o mapa

--- tilemap.py
+++ tilemap.py
@@ -5,7 +5,7 @@ Created on Fri May 17 14:20:45 2019
 
 @author: beatrizcf
 """
-
+import pytmx
 import pygame as pg
 import settings
 
@@ -24,6 +24,26 @@ class Map:
         self.width = self.tilewidth * settings.TILESIZE
         self.height = self.tileheight * settings.TILESIZE
 
+class TiledMap:
+    def __init__ (self,filename):
+        tm = pytmx.load_pygame(filename, pixelalpha=True)
+        self.width = tm.width*tm.tilewidth
+        self.height = tm.height*tm.tileheight
+        self.tmxdata = tm
+        
+    def render(self, surface):
+        ti = self.tmxdata.get_tile_image_by_gid
+        for layer in self.tmxdata.visible_layers:
+            if isinstance (layer, pytmx.TiledTileLayer):
+                for x,y,gid, in layer:
+                    tile = ti(gid)
+                    if tile:
+                        surface.blit(tile, (x * self.tmxdata.tilewidth, y * self.tmxdata.tileheight))
+    def make_map(self):
+        temp_surface = pg.Surface((self.width, self.height))
+        self.render(temp_surface)
+        return temp_surface
+    
 class Camera:
     def __init__(self, width, height):
         self.camera = pg.Rect(0, 0, width, height)
@@ -32,6 +52,9 @@ class Camera:
 
     def apply(self, entity):
         return entity.rect.move(self.camera.topleft)
+    
+    def apply_rect(self,rect):
+        return rect.move(self.camera.topleft)
 
     def update(self, target):
         x = -target.rect.centerx + int(settings.WIDTH / 2)
