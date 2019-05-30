0128650feac9545e7cff711248a161979c431602
PedroHMenezes <49311416+PedroHMenezes@users.noreply.github.com>
2019-05-29 23:16:18 -0300

    Esqueci de subir alguns arquivos

---
+++ map/labirinto.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="labirinto" tilewidth="16" tileheight="16" spacing="2" tilecount="16198" columns="14">
+ <image source="../../Map/pokemon tileset.png" width="256" height="20832"/>
+</tileset>
