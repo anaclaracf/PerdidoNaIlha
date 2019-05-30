cb24badfcb17fcb2967e70ab2c8fa7badea05eb0
PedroHMenezes <49311416+PedroHMenezes@users.noreply.github.com>
2019-05-30 12:56:39 -0300

    Pequenos ajustes para apresentar

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class game_intro:
 
 def draw_text(surf,text,size,x,y):
     font=pg.font.Font(font_name,size)
-    text_surface=font.render(text,True,settings.WHITE)
+    text_surface=font.render(text,True,settings.BLACK)
     text_rect=text_surface.get_rect()
     text_rect.midtop=(x,y)
     surf.blit(text_surface, text_rect)
