4f6e88868d59ab4c504b9e7e500d317159ecada2
PedroHMenezes <49311416+PedroHMenezes@users.noreply.github.com>
2019-05-31 15:11:47 -0300

    Arrumando labirinto

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ def message_display(text,pg):
     gameDisplay.blit(TextSurf, TextRect)
     pg.display.update()
     time.sleep(2)
-    game_loop()
 
                             
 class game_intro:
@@ -71,13 +70,6 @@ class game_intro:
             g.run()   
             g.show_go_screen()
 
-def draw_text(surf,text,size,x,y):
-    font=pg.font.Font(font_name,size)
-    text_surface=font.render(text,True,settings.BLACK)
-    text_rect=text_surface.get_rect()
-    text_rect.midtop=(x,y)
-    surf.blit(text_surface, text_rect)
-    
 class Game:
     def __init__(self):
         pg.init()
